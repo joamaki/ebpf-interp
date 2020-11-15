687e9bec6f1d1b4ff1c7a855df3d6e8d0d631c4b
Jussi Maki <joamaki@gmail.com>
2020-11-15 15:20:23 +0100

    Properly handle ip header size

--- progs/tcp-port-filter.c
+++ progs/tcp-port-filter.c
@@ -5,7 +5,7 @@ uint64_t test(struct frame *frame) {
 	struct ethhdr *ehdr = (struct ethhdr*)frame->data;
 	if (ehdr->proto == HTONS(ETHPROTO_IP)) {
 		struct iphdr *iphdr = (struct iphdr*)(frame->data + sizeof(struct ethhdr));
-		uint8_t iphdr_size = sizeof(struct iphdr); //(iphdr->version_and_ihl & 0x0f) * 4;
+		uint8_t iphdr_size = (iphdr->version_and_ihl & 0x0f) * 4;
 		if (iphdr->protocol == IPPROTO_TCP) {
                 	struct tcphdr *tcphdr = (struct tcphdr*)(frame->data + sizeof(struct ethhdr) + iphdr_size);
                 	return tcphdr->dport == HTONS(22);
